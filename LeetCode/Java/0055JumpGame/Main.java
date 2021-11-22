76cb1aac397dec159526f54dfa30db1c7c636802
Alexander Kourkoumelis <alexanderkourkoumelis@Alexanders-MacBook-Pro.local>
2021-11-21 20:17:13 -0800

    refined 0055JumpGame

--- LeetCode/Java/0055JumpGame/Main.java
+++ LeetCode/Java/0055JumpGame/Main.java
@@ -2,34 +2,25 @@ public class Main {
     public static void main(String[] args) {
         int[] nums0 = {2, 3, 1, 1, 4};
         System.out.println("true:" + Solution.canJump(nums0));
+        int[] nums1 = {1, 2, 3};
+        System.out.println("true:" + Solution.canJump(nums1));
+        int[] nums2 = {3, 2, 1, 0, 4};
+        System.out.println("false:" + Solution.canJump(nums2));
     }
 }
 
-
 class Solution {
     public static boolean canJump(int[] nums) {
-        int[] jumps = new int[nums.length];
+        int maxJumps = nums[0];
 
-        if (nums.length < 2)
-            return true;
-        if (nums[0] == 0)
-            return false;
-
-        int maxJumpLength = nums[0];
-
-        for (int i = 1; i < nums.length - 1; i++) {
-            maxJumpLength--;
-            if (maxJumpLength < nums[i]) {
-                maxJumpLength = nums[i];
-            }
-            if (maxJumpLength == 0)
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxJumps--;
+            if (maxJumps < nums[i])
+                maxJumps = nums[i];
+            if (maxJumps == 0)
                 return false;
         }
-
-
 
         return true;
     }
 }
-
-
