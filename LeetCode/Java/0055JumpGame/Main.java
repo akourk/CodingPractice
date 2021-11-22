0ce0b0c8e9a6d723349d5dc8bce720c054efc7ad
Alexander Kourkoumelis <alexanderkourkoumelis@Alexanders-MacBook-Pro.local>
2021-11-21 20:37:31 -0800

    0055JumpGame is now dynamic with the old solution in comments

--- LeetCode/Java/0055JumpGame/Main.java
+++ LeetCode/Java/0055JumpGame/Main.java
@@ -6,21 +6,37 @@ public class Main {
         System.out.println("true:" + Solution.canJump(nums1));
         int[] nums2 = {3, 2, 1, 0, 4};
         System.out.println("false:" + Solution.canJump(nums2));
+        int[] nums3 = {0, 2, 3};
+        System.out.println("false:" + Solution.canJump(nums3));
     }
 }
 
 class Solution {
     public static boolean canJump(int[] nums) {
-        int maxJumps = nums[0];
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            maxJumps--;
-            if (maxJumps < nums[i])
-                maxJumps = nums[i];
-            if (maxJumps == 0)
-                return false;
+        int maxReachable = nums[0];
+        int i = 0;
+        while (i <= maxReachable) {
+            maxReachable = Math.max(maxReachable, i + nums[i]);
+            if (maxReachable >= nums.length - 1)
+                return true;
+            i++;
         }
-
-        return true;
+        return false;
     }
 }
+
+// class Solution {
+//     public static boolean canJump(int[] nums) {
+//         int maxJumps = nums[0];
+
+//         for (int i = 0; i < nums.length - 1; i++) {
+//             maxJumps--;
+//             if (maxJumps < nums[i])
+//                 maxJumps = nums[i];
+//             if (maxJumps == 0)
+//                 return false;
+//         }
+
+//         return true;
+//     }
+// }
