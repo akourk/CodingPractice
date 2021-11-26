4006a90bca84d17e88cc8e48378a29e02871c387
Alexander Kourkoumelis <alexanderkourkoumelis@Alexanders-MacBook-Pro.local>
2021-11-26 15:23:17 -0800

    adding leetcode 0139 Word Break

---
+++ LeetCode/Java/0139WordBreak/Main.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "leetcode";
+        List<String> l1 = new ArrayList();
+        l1.add("leet");
+        l1.add("code");
+        System.out.println("true:" + Solution.wordBreak(s1, l1));
+        
+    }
+}
+
+class Solution {
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[dp.length - 1] = true;
+
+        for (int i = s.length(); i >= 0; i--) {
+            for (String word : wordDict) {
+                if (i + word.length() - 1 <= s.length() - 1 
+                && s.substring(i, i + word.length()).equals(word))
+                    dp[i] = dp[i + word.length()];
+                if (dp[i])
+                    break;
+            }
+        }
+        return dp[0];
+    }
+}
+
+// this is cleaner and faster. always use a hashset...
+// class Solution {
+//     public static boolean wordBreak(String s, List<String> wordDict) {
+//         Set<String> hs = new HashSet<>(wordDict);
+//         boolean[] dp = new boolean[s.length() + 1];
+//         dp[0] = true;
+//         for (int i = 1; i <= s.length(); i++) {
+//             for (int j = 0; j <i; j++) {
+//                 if (dp[j] && hs.contains(s.substring(j, i))) {
+//                     dp[i] = true;
+//                     break;
+//                 }
+//             }
+//         }
+//         return dp[s.length()];
+//     }
+// }
