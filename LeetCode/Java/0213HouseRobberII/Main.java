151cfb2d2f450f2e2ccb2591be996f0a217182f7
akourk <akourk@icloud.com>
2021-12-05 23:37:10 -0800

    adding leetcode 0213HouseRobberII

--- LeetCode/Java/0213HouseRobberII/Main.java
+++ LeetCode/Java/0213HouseRobberII/Main.java
@@ -1,15 +1,26 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] nums0 = {2, 3, 2};
         int[] nums1 = {1, 2, 3, 1};
+        int[] nums2 = {0};
         System.out.println("3:" + Solution.rob(nums0));
         System.out.println("4:" + Solution.rob(nums1));
-
+        System.out.println("0:" + Solution.rob(nums2));
     }
 }
 
 class Solution {
     public static int rob(int[] nums) {
+        if (nums.length == 1)
+            return nums[0];
+        if (nums.length == 2)
+            return Math.max(nums[0], nums[1]);
+        return Math.max(robfunc(Arrays.copyOfRange(nums, 0, nums.length - 1)), 
+            robfunc(Arrays.copyOfRange(nums, 1, nums.length)));
+    }
+    private static int robfunc(int[] nums) {
         int[] max = new int[nums.length + 1];
         
         max[0] = 0;
