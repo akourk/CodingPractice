28528e747178596242bddfb05262145dda472e80
akourk <akourk@icloud.com>
2022-03-05 21:14:50 -0800

    adding leet code 0023 Merge K Sorted Lists

---
+++ LeetCode/Java/0023MergeKSortedLists/Main.java
@@ -0,0 +1,32 @@
+class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode ln = new ListNode();
+
+        return ln;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
